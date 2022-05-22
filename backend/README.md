ec264a6b786b0da512f3623907216e4088ca3db5
Kimi Päivärinta <mail@kimin.info>
2022-05-22 11:39:18 +0300

    Setting up backend with azure functions + fastapi with Python vs code config, linting, formatting

---
+++ backend/README.md
@@ -0,0 +1,7 @@
+## Dev environment
+
+Requirements:
+* VS Code
+* Azure Functions Core Tools (https://docs.microsoft.com/en-us/azure/azure-functions/create-first-function-vs-code-python)
+* Python 3.9
+* Enable venv and install dev-requirements
---
+++ backend/fastapi/__init__.py
@@ -0,0 +1,18 @@
+import azure.functions as func
+
+import fastapi
+
+app = fastapi.FastAPI()
+
+
+@app.get("/hello/{name}")
+def get_name(
+    name: str,
+):
+    return {
+        "name": name,
+    }
+
+
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    return func.AsgiMiddleware(app).handle(req, context)
