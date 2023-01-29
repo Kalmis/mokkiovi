3ff35fc95ba34978b23c04d86b2716858ae2803f
Kimi Päivärinta <kimi.paivarinta@kaikuhealth.com>
2023-01-30 00:28:56 +0200

    Get access token from localstorage when making a call

--- mokkiovi/src/openapi/Mokkiovi.ts
+++ mokkiovi/src/openapi/Mokkiovi.ts
@@ -6,6 +6,7 @@ import type { OpenAPIConfig } from './core/OpenAPI';
 import { AxiosHttpRequest } from './core/AxiosHttpRequest';
 
 import { DefaultService } from './services/DefaultService';
+import { ApiRequestOptions } from './core/ApiRequestOptions';
 
 type HttpRequestConstructor = new (config: OpenAPIConfig) => BaseHttpRequest;
 
@@ -32,3 +33,31 @@ export class Mokkiovi {
     }
 }
 
+const getAccessToken = async(options: ApiRequestOptions): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        const storageToken = localStorage.getItem('token');
+        if (storageToken) {
+            resolve(storageToken)
+        }
+        else {
+            resolve('')
+        }
+    })
+
+}
+
+const BackendConfig: OpenAPIConfig =  {
+    BASE: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000',
+    VERSION: '0.1.0',
+    WITH_CREDENTIALS: false,
+    CREDENTIALS: 'include',
+    TOKEN: getAccessToken,
+    USERNAME: undefined,
+    PASSWORD: undefined,
+    HEADERS: undefined,
+    ENCODE_PATH: undefined,
+} 
+
+const MokkioviService = new Mokkiovi(BackendConfig)
+
+export default MokkioviService
